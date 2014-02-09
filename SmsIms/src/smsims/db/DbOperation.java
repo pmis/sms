ad1be7edf72700e3a4cf0b77a21c653e590eb462
Lasith Chandrasekara <lasithc@gmail.com>
2014-02-09 18:46:54 +0530

    adding the first commit

---
+++ SmsIms/src/smsims/db/DbOperation.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package smsims.db;
+
+import java.util.Properties;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import smsims.om.Member;
+
+/**
+ *
+ * @author Lasith.Chandrasekara
+ */
+public class DbOperation {
+    private static SessionFactory sessionFactory = null;  
+    private static ServiceRegistry serviceRegistry = null;  
+
+    public DbOperation() {
+        // Configure the session factory
+        if (sessionFactory == null) {
+            configureSessionFactory();            
+        }
+    }
+        
+    private static SessionFactory configureSessionFactory() throws HibernateException {  
+        Configuration configuration = new Configuration();  
+        configuration.configure();  
+         
+        Properties properties = configuration.getProperties();         
+        serviceRegistry = new ServiceRegistryBuilder().applySettings(properties).buildServiceRegistry();          
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);  
+         
+        return sessionFactory;  
+    }
+        
+    public void insertMember(Member member) throws Exception{
+        Session session = null;
+        Transaction tx = null;
+         
+        try {
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();            
+            
+            // Saving to the database
+            session.saveOrUpdate(member);
+             
+            // Committing the change in the database.
+            session.flush();
+            tx.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+             
+            // Rolling back the changes to make the data consistent in case of any failure 
+            // in between multiple database write operations.
+            tx.rollback();
+            throw ex;
+        } finally{
+            if(session != null) {
+                session.close();
+            }
+        }
+    }
+    
+        
+}
---
+++ SmsIms/src/smsims/om/Member.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package smsims.om;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Lasith.Chandrasekara
+ */
+@Entity
+@Table(name = "member_details")
+public class Member {
+    @Id @GeneratedValue
+    private Integer id;
+    
+    private String name;
+    
+    private String tpNumber;
+    
+    private String department;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the tpNumber
+     */
+    public String getTpNumber() {
+        return tpNumber;
+    }
+
+    /**
+     * @param tpNumber the tpNumber to set
+     */
+    public void setTpNumber(String tpNumber) {
+        this.tpNumber = tpNumber;
+    }
+
+    /**
+     * @return the department
+     */
+    public String getDepartment() {
+        return department;
+    }
+
+    /**
+     * @param department the department to set
+     */
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    
+}
